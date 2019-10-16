4286656b4831f397a43297427e4eb14d91ef023d
shantanu.sadar <shantanu.sadar@cognizant.com>
2019-10-16 22:19:01 +0530

    Add all files

--- shopping-cart/src/app/app.module.ts
+++ shopping-cart/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { OrderListComponent } from './order-list/order-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    OrderListComponent
   ],
   imports: [
     BrowserModule,
