ae1962983ec61a380246cfed6767e6344cde5a16
shantanu.sadar <shantanu.sadar@cognizant.com>
2019-10-19 06:39:18 +0530

    removed component to rename

--- shopping-cart/src/app/app.module.ts
+++ shopping-cart/src/app/app.module.ts
@@ -3,16 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ProductsComponent } from './products/products.component';
-import { OrderListComponent } from './order-list/order-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    ProductsComponent,
-    OrderListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
